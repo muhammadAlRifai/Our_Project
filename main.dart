c8f0313cafb50809ef900a0f366822f41ee4e83f
muhammadAlRifai <mosomosobi2018630674@gmail.com>
2022-07-22 06:11:40 +0300

    eng Has Call quizApp() function at Main.dart File

--- main.dart
+++ main.dart
@@ -1,4 +1,8 @@
+import 'quiz_app.dart';
+
 void main(List<String> args) {
   print('Hello From Muhammad');
   print('This Is First Commit while Creating The Our_Project Repository');
+
+  quizApp();
 }
